5124065841d34fb7c97ffad1c404b9b6fd0712f4
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2024-01-30 11:33:59 +0000

    Use PostgresNIO directly

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,33 +1,23 @@
-import NIOSSL
-import Fluent
-import FluentPostgresDriver
 import Vapor
+import PostgresNIO
 
 public func configure(_ app: Application) async throws {
     let password = "YOUR PASSWORD" // password for postgres user
     let tenant = "YOUR PROJECT REFERENCE" // project reference ID
     
-    let templateWorking = "postgresql://postgres:\(password)@db.\(tenant).supabase.co:5432/postgres"
-    let templateFailing = "postgresql://postgres.\(tenant):\(password)@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
-    
-    // Change this to toggle Connection Pool on/off (pooler).
-    let failing = true
-    
-    guard let url = URL(string: failing ? templateFailing : templateWorking) else {
-        print("URL failed to construct")
-        return
-    }
-    
-    app.logger.info("Using database with URL: \(url.absoluteString)")
-    
-    var postgresConfig = try SQLPostgresConfiguration(url: url)
-    postgresConfig.coreConfiguration.tls = .disable
-    app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    let logger = Logger(label: "postgres-logger")
+    let config = PostgresConnection.Configuration(
+        host: "aws-0-eu-west-2.pooler.supabase.com",
+        port: 6543,
+        username: "postgres.\(tenant)",
+        password: password,
+        database: "postgres",
+        tls: .disable
+    )
     
     do {
-        let db = app.db as! PostgresDatabase
-        let rows = try await db.simpleQuery("SELECT version();").get()
-        print(rows) // if this gets executed, it worked fine
+        let connection = try await PostgresConnection.connect(on: app.eventLoopGroup.next(), configuration: config, id: 1, logger: logger)
+        print(connection)
     } catch {
         print("\n\nFailed to execute...\n\n🚨 \(String(reflecting: error))\n\n")
     }
